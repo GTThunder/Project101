34fce5c8b3a82e4b694639248dad7fcb3875ae79
Daryl Tan <175454x@mymail.nyp.edu.sg>
2017-12-01 21:54:38 +0800

    Initial commit

---
+++ Home.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate('cred/daryltan-9eddf-firebase-adminsdk-gj8gk-a7e6e9d435.json')
+default_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://daryltan-9eddf.firebaseio.com/'
+})
+
+root = db.reference()
+
+app = Flask(__name__)
+
+@app.route('/home/')
+def home():
+    return render_template('home.html')
+
+@app.route('/mt/')
+def mrtTiming():
+    return render_template('mrtTiming.html')
+
+@app.route('/at/')
+def at():
+    return render_template('alternative_transport.html')
+
+if __name__ == '__main__':
+    app.secret_key = 'secret123'
+    app.run()
+
+
