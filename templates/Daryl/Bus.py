58b23410f82e189c860daeb493423e4aef44d211
Daryl Tan <175454x@mymail.nyp.edu.sg>
2017-12-12 22:03:04 +0800

    Daryl's files

--- templates/Daryl/Bus.py
+++ templates/Daryl/Bus.py
@@ -1,69 +1,41 @@
-from templates.Daryl.Train import Train
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.math = 0
+        self.chinese = 0
+        self.english = 0
+        self.science = 0
+        self.choices = []
 
-class Timing(Train):
-
-    def __init__(self, station, exits, type_of_line, timing, direction):
-        Train.__init__(self, station, exits, type_of_line)
-        self.__timing = timing
-        self.__direction = direction
-
-    def get_timing(self):
-        return self.__timing
-
-    def get_direction(self):
-        return self.__direction
-
-    def set_timing(self, timing):
-        self.__timing = timing
-
-    def set_direction(self, direction):
-        self.__direction = direction
+    def get_score(self):
+        return (self.math + self.chinese + self.english + self.science) / 4
 
     def __str__(self):
-        return 'Name of station is {}, type of line is {}, estimated arrival of train is {}, ' \
-               'direction heading towards is {}'.format(self.get_station(), self.get_type_of_line(),
-                self.__timing, self.__direction)
+        return '{} scores {}, the choices are {}, {}, {}'.format(self.name,
+                    self.get_score(), self.choices[0], self.choices[1], self.choices[2])
 
-class Time:
-    timing = []
-    choice = []
+def main():
+    students = load_result()
+    for s in students:
+        s.choices.append('SchoolA')
+        s.choices.append('SchoolB')
+        s.choices.append('SchoolC')
 
-    def __init__(self):
-        self.create_timing()
+    for s in students:
+        print(s)
 
-    def create_timing(self):
-        t1 = Timing('Bishan', 'A', 'Circle Line', '3 mins', 'Towards HabourFront')
-        self.add_timing(t1)
-        t2 = Timing('Bishan', 'B', 'Circle Line', '5 mins', 'Towards Dhoby Ghaut')
-        self.add_timing(t2)
-        t3 = Timing('Bishan', 'C', 'North South Line', '3 mins', 'Marina Bay')
-        self.add_timing(t3)
-        t4 = Timing('Bishan', 'D', 'North South Line', '5 mins', 'Jurong East')
-        self.add_timing(t4)
+def load_result():
+    students = []
+    result_file = open('results.txt', 'r')
+    for result in result_file:
+        list = result.split(',')
+        s = Student(list[0])
+        s.math = float(list[1])
+        s.chinese = float(list[2])
+        s.english = float(list[3])
+        s.science = float(list[4])
+        students.append(s)
+    return students
 
-    def add_timing(self, time):
-        self.__class__.timing.append(time)
-
-    def show_timing(self):
-        while True:
-            number = 1
-            for time in self.__class__.timing:
-                print(number, time)
-                number += 1
-            choice = int(input('Enter your choice (1,2,3,4): '))
-            if choice == 1 or 2 or 3 or 4:
-                time = self.__class__.timing[choice - 1]
-                self.__class__.choice.append(time)
-                break
-
-    def display_choices(self):
-        print('You have chosen:')
-        for order in self.__class__.choice:
-            print(order)
-
-
-x = Time()
-x.show_timing()
-x.display_choices()
-
+main()
 
